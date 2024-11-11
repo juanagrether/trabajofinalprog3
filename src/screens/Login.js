565aacac1e44b09258d96fd74b9c748e48e0a19b
juanagrether <jgrether@udesa.edu.ar>
2024-11-11 18:19:49 -0300

    comenzando login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,32 +1,79 @@
-import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import  React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 class Login extends Component {
-  render() {
-    return (
-      <View style={styles.container} >
-        <Text style={styles.title}>Ingresar</Text>
-        <Text style={styles.description}>Formulario de LOGIN: navegacion cruzada con el Registro</Text>
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          email: '',
+          password: '',
+        };
+    }
 
-        <TouchableOpacity 
-          style={styles.buttonBlue} 
-          onPress={() => this.props.navigation.navigate('Register')}
-        >
-          <Text style={styles.buttonText}>No tienes cuenta? REGISTER!</Text>
-        </TouchableOpacity>
+	render() {
+        const { navigation } = this.props; // Accediendo a las props de navegación
 
-        <Text style={styles.description}>Navegacion cruzada con ingresar a la APP</Text>
+        return (
+            <View style={styles.container} >
+                <Text style={styles.title}> Ingresar </Text>
 
-       
-        <TouchableOpacity 
-          style={styles.buttonOrange} 
-          onPress={() => this.props.navigation.navigate('HomeMenu')}
-        >
-          <Text style={styles.buttonText}>Entrar en la app</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+                <Text style={styles.description}> Formulario de LOGIN : navegacion cruzada con el Registro </Text>
+                <TouchableOpacity style={styles.buttonBlue} onPress={ ()=> this.props.navigation.navigate('Register')}>
+                    <Text style={styles.buttonText}>No tienes cuenta? REGISTER!</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.description}> Navegacion cruzada con ingresar a la APP </Text>
+                <TouchableOpacity style={styles.buttonOrange} onPress={() => navigation.navigate('HomeMenu')}>
+                    <Text style={styles.buttonText}>Entrar en la app</Text>
+                </TouchableOpacity>
+                
+            </View>
+        )
+    }
 }
 
-export default Login;
+export default Login; 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      backgroundColor: '#f5f5f5',
+    },
+    title: {
+      fontSize: 32,
+      fontWeight: 'bold',
+      margin: 20,
+    },
+    description: {
+      fontSize: 16,
+      textAlign: 'center',
+    },
+    buttonBlue: {
+      backgroundColor: '#67b7f7',
+      padding: 15,
+      margin: 20,
+      borderRadius: 10,
+      width: 600,
+      height:50,
+      alignItems: 'center',
+    },
+    buttonOrange: {
+      backgroundColor: '#f7a667',
+      padding: 15,
+      margin: 20,
+      borderRadius: 10,
+      width: 600,
+      height:50,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+  });
