b78dd495b81d0708fcd7beabc5f55a381264b351
valentinaCortigiani <vcortigiani@udesa.edu.ar>
2024-11-11 18:18:15 -0300

    continuacion navigation

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -2,8 +2,6 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "../screens/Home";
-import Login from "../screens/Login";
-import Register from "../screens/Register";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +10,6 @@ function HomeMenu() {
         
         <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Login" component={Login} />
-            <Tab.Screen name="Register" component={Register} />
         </Tab.Navigator>
     );
 }
